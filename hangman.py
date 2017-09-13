ea958652097baab9dc1fbbcf9247f4c5e6d003c9
Anthony Albertorio <anthony.albertorio@gmail.com>
2017-09-13 14:58:47 -0400

    added visualization for game to show hangman. Set to 6 tries. Need better way to make it variable amount of tries.

--- hangman.py
+++ hangman.py
@@ -1,9 +1,7 @@
 # re-factor:
-# extract random word choice code
-# code to extract case correction code outside word update
+# re-factor extract random word choice code
+# re-factor code to extract case correction code outside word update
 # draw hangman function
-# add ability to query dictionary api to get random word.
-
 """
 Hangman in Python (beta)
 by Anthony Albertorio
@@ -12,6 +10,53 @@ by Anthony Albertorio
 import random
 
 words = ['Hello', 'world', 'United', 'America', 'Korea', 'cars', 'jazz']
+
+
+def hangman_draw(count):
+    head = '            |'
+    torso = '               |'
+    left_arm = ''
+    right_arm = '     '
+    left_leg_1 = '              |'
+    left_leg_2 = '              |'
+    right_leg_1 = ' '
+    right_leg_2 = '  '
+
+    if count >= 1:
+        head = 'O           |'
+    if count >= 2:
+        torso = '   |           |'
+    if count >= 3:
+        torso = '|           |'
+        left_arm = 'o--'
+    if count >= 4:
+        torso = '|'
+        left_arm = 'o--'
+        right_arm = '--o        |'
+    if count >= 5:
+        left_leg_1 = ' /            |'
+        left_leg_2 = '/             |'
+    if count >= 6:
+        left_leg_1 = ' /'
+        left_leg_2 = '/'
+        right_leg_1 = '\\          |'
+        right_leg_2 = '  \\         |'
+
+    print("""
+         ____________ 
+        |           |
+        {0}
+     {2}{1}{3}
+      {4} {6}
+      {5} {7}
+                    |
+                    |
+               _________
+              |         |
+              |_________|
+    """.format(head, torso, left_arm, right_arm,
+               left_leg_1, left_leg_2,
+               right_leg_1, right_leg_2))
 
 
 def get_random_word(word_list):
@@ -52,7 +97,7 @@ def word_update(hidden_word, random_word, guess_letter):
         # replace letter
         if len(matched_values) != 0:
             for letters in matched_values:
-                # problem is here
+                # probelm is here
                 hidden_word_list[letters] = guess_letter
             # place back letter case
             for upper_case in upper_case_list:
@@ -75,10 +120,14 @@ def word_update(hidden_word, random_word, guess_letter):
 
 def game(word_list):
     random_word = get_random_word(word_list)
-    guess_count = 5
+    guess_count = 6
+    countdown = 0
     hidden_word = '_' * len(random_word)
     letters_inputed_list = []
     output_word = space_maker(hidden_word)
+
+    # output initial word hint
+    print('{0} letters: {1}'.format(len(hidden_word), output_word))
 
     while guess_count > 0:
         # reveals word to test
@@ -88,8 +137,8 @@ def game(word_list):
             print('All done!')
             break
 
-        # output initial word hint
-        print('\n{0} letters: {1}'.format(len(hidden_word), output_word))
+        # draw
+        hangman_draw(countdown)
         # get input
         user_guess = input('Guess a letter: ')
         if user_guess not in letters_inputed_list:
@@ -97,13 +146,17 @@ def game(word_list):
             if word_update(hidden_word, random_word, user_guess) != None:
                 hidden_word = word_update(hidden_word, random_word, user_guess)
                 output_word = space_maker(hidden_word)
+                # draw
+                hangman_draw(countdown)
                 print(output_word)
             else:
                 guess_count -= 1
+                # counter up letter
+                countdown += 1
                 print('You have {0} guesses'.format(guess_count))
         else:
-            print('Try again. letter there already\n')
-    else:
-        print('You lost!\n')
+            print('You guessed that letter. Try again.')
+
 
 game(words)
+
